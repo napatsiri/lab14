1daf0c7b30b57865f2e75bb842b9aeb2fac4a4ad
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2021-12-28 16:58:23 +0700

    lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,28 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool p[][M], int x, int y, int z){
+    for(int a = 0; a < N; a ++){
+        for(int b = 0; b < M; b++){
+            if(sqrt(pow(a-y,2)+pow(b-z,2))+1 <= x){
+                p[a][b] = 1;
+            }
+        }
+    }
+}
+
+void showImage(const bool p[][M]){
+
+     cout << "------------------------------------------------------------------------\n";
+    for(int m = 0; m < N; m++){
+        cout << "|";
+        for(int o = 0; o < M ; o++){
+            if(p[m][o] == 0) cout << " ";
+            if(p[m][o] == 1) cout << "*";
+        }
+        cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------\n";
+
+}
